d944902aebb5665336f42c6ff595f70fb30b8b35
David Henry <dw_henry@yahoo.com.au>
2013-06-03 22:31:07 +0100

    Make everything work with Zeus

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,9 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+unless ENV['ZEUS_MASTER_FD']
+  require 'rspec/autorun'
+end
 require 'capybara/rails'
 require 'ruby-debug'
 # Requires supporting ruby files with custom matchers and macros, etc,
