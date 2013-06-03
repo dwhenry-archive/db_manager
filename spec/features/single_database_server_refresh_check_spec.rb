d2c3df0c306ba60e1a34dcc66133ca13eccf3f93
David Henry <dw_henry@yahoo.com.au>
2013-06-03 23:47:03 +0100

    Move spec DSL methods into own file

--- spec/features/single_database_server_refresh_check_spec.rb
+++ spec/features/single_database_server_refresh_check_spec.rb
@@ -4,51 +4,6 @@ feature %{
   When Querying by a specific database name
 } do
 
-  module Builder
-    class ServerSet
-      def initialize(name)
-        @server = ::ServerSet.create(name: name, server_type: 'DbServer')
-      end
-
-      Date::DAYNAMES.each do |day_name|
-        define_method :"enable_#{day_name.downcase}" do
-          @server.settings.create(key: day_name.downcase, value: 'true')
-        end
-      end
-
-      def enable_round_robin
-        @server.settings.create(key: 'round_robin', value: 'true')
-      end
-
-      def set_last_run(date)
-        Timecop.travel(date) do
-          @server.logs.create(action: 'db_update_complete', source: 'test')
-        end
-      end
-    end
-  end
-
-  def create_db_server(name)
-    yield Builder::ServerSet.new(name)
-  end
-
-  def check_db_server(name)
-    visit db_server_check_path(name)
-    within('body') { return page.html }
-  end
-
-  def travel_to_monday
-    Timecop.travel(Date.today - Date.today.wday + 1) do
-      yield
-    end
-  end
-
-  def travel_to_sunday
-    Timecop.travel(Date.today - Date.today.wday ) do
-      yield
-    end
-  end
-
   scenario 'Return 404 when server is not configured' do
     expect do
       check_db_server('invalid_server')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ unless ENV['ZEUS_MASTER_FD']
 end
 require 'capybara/rails'
 require 'ruby-debug'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -38,4 +39,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  config.include Builder
 end
